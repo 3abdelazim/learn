dc5cd474d076f187ea28a9836de26f9c23c92404
Mohamed Sherif <3abdelazim@gmail.com>
2019-01-24 13:56:04 +0000

    change welcome message

--- api/src/index.js
+++ api/src/index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 
 app.get('/api', (req, res) => {
-  res.json({ text: 'Hello Docker & nginx!' })
+  res.json({
+    text: 'Hello world! From node.js, react.js, nginx, and docker; for now'
+  })
 })
 
 app.listen(80, () => {})
-
